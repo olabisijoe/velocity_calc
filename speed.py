e105c524f820126792dc273aa1da2551e26f0b14
Olabisi Olarewaju <olarewajuolabisi62@gmail.com>
2024-04-25 15:59:31 -0500

    First commit

---
+++ speed.py
@@ -0,0 +1,7 @@
+# Code for users to input distance  and time
+distance = float(input("Whats's your distance in metre "))
+time = float(input("What's your time in seconds "))
+
+# Formula for velocity is V=d/t
+velocity = distance/time 
+print("The velocity is: ",velocity, "m/s")
